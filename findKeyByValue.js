6027bab6659e0755552a6b7a4ad4bda264f4c1d6
Josh Sparkes <jshsparx@gmail.com>
2022-03-14 15:48:44 -0600

    refactored index.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -33,7 +33,7 @@ module.exports = findKeyByValue;
 //   drama:  "The Wire"
 // };
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual (findKeyByValue(bestTVShowsByGenre, "The Office"), "comedy");
-assertEqual (findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual (findKeyByValue(bestTVShowsByGenre, "The Office"), "comedy");
+// assertEqual (findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
